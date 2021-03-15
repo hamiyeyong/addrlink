ae3840c8bddfbd8bce9bbe3cedf8813e26bd57a8
hamiyeyong <hamiyeyong@gmail.com>
2021-03-15 16:28:40 +0900

    검색 컴포넌트 스타일링 1차 완료

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { MdSearch } from "react-icons/md";
+import "./Search.scss";
 
 function Search() {
-  return <div>주소를 입력해주세요.</div>;
+  return (
+    <>
+      <h1>영문 주소 변환</h1>
+      <div className="Search">
+        <input placeholder="주소입력" />
+        <MdSearch className="icon" />
+      </div>
+    </>
+  );
 }
 
 export default Search;
